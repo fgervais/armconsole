faa14c5bcadbb64cdcdef30ed35ded1ff8964dfb
FrancoisGervais <FrancoisGervais@a08e5c7e-efe4-11de-a198-37193048d1ed>
2010-02-26 00:30:13 +0000

    SDRAM and LCD are both working. We now have a lot of RAM to play with!

--- src/Application.cpp
+++ src/Application.cpp
@@ -42,12 +42,148 @@ int main() {
 	LCDControllerDriver* lcd = LPC2478::getLCD();
 	lcd->configure(lcdConfig);
 
+	led->setLow();
+
+	uint32_t* lcd_ptr = (uint32_t*)0xA0000000;
+	uint32_t bufferLength = 480*272;
+	uint32_t r,g,b;
+
+	/*for (uint32_t i=0; i<bufferLength; i++) {
+		*(lcd_ptr++) = 0x00FFFF00 | (i & 0xFF);
+	}
+	while(1);*/
+
+	r=255;
+	g=0;
+	b=0;
+	while(1) {
+		for(;g<251;g+=4) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;r>8;r-=8) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;b<247;b+=8) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;g>4;g-=4) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;r<247;r+=8) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;b>8;b-=8) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+	}
+
+	r=255;
+	g=0;
+	b=0;
+	while(1) {
+		for(;g<255;g+=1) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;r>0;r-=1) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;b<255;b+=1) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;g>0;g-=1) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;r<255;r+=1) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+		for(;b>0;b-=1) {
+			lcd_ptr = (uint32_t*)0xA0000000;
+			for (uint32_t i=0; i<bufferLength; i++) {
+				*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+			}
+		}
+	}
+
+
+	while(1) {
+		for(r=0; r<255; r+=16) {
+			for(g=0; g<255; g+=1) {
+				for(b=0; b<255; b+=1) {
+					lcd_ptr = (uint32_t*)0xA0000000;
+					for (uint32_t i=0; i<bufferLength; i++) {
+						*(lcd_ptr++) = (b << 16) | (g << 8) | r;
+						//LPC2478::delay(10);
+					}
+
+				}
+
+			}
+		}
+	}
+
+
+	// Test sdram
+#define SDRAM_BASE_ADDR *(volatile unsigned int*)0xA0000000
+
+	/*unsigned int i;
+	for (i = 0; i < 0x2000000; i+=sizeof(unsigned int))
+	{
+	*(unsigned int *)((unsigned int )&SDRAM_BASE_ADDR+i) = i;
+	}
+
+	for (i = 0; i < 0x2000000; i+=sizeof(unsigned int ))
+	{
+	if (*(unsigned int *)((unsigned int )&SDRAM_BASE_ADDR+i) != i)
+	{
+	  led->setHigh();
+	  while(1);
+	}
+	}*/
+
+	//while(1);
+	//LPC2478::delay(1000000);
+
 	// Blink led
 	while(1) {
 		led->setHigh();	// On
-		for(uint32_t i=0; i<100000; i++);
+		//for(uint32_t i=0; i<100000; i++);
+		LPC2478::delay(1000000);
 		led->setLow();	// Off
-		for(uint32_t i=0; i<100000; i++);
+		//for(uint32_t i=0; i<100000; i++);
+		LPC2478::delay(1000000);
 	}
 
 }
--- src/lowlevel/src/target.c
+++ src/lowlevel/src/target.c
@@ -136,9 +136,161 @@ void TargetResetInit(void)
 
 	init_VIC();
 
-	// TODO: Config SDRAM
+	ConfigureSDRAM();
 
     return;
+}
+
+void ConfigureSDRAM(void) {
+	// Code below are modified from AN10771 from NXP
+	DWORD wtemp;
+	volatile int i;
+
+	// Start EMC clock
+	PCONP |= (1 << 11);
+
+	PINSEL6 |= 0x55555555;		// D0-D15
+	PINSEL7 |= 0x55555555;		// D16-D31
+	PINSEL8 |= 0x15555555;		// A0-A14
+	PINSEL9 |= (1 << 18);		// WE
+	PINSEL5 |= (1 << 0);		// CAS
+	PINSEL5 |= (1 << 2);		// RAS
+	PINSEL5 |= (1 << 4);		// CLKOUT0
+	PINSEL5 |= (1 << 8);		// DYCS0
+	PINSEL5 |= (1 << 16);		// CKEOUT0
+	PINSEL5 |= (1 << 24);		// DQMOUT0
+	PINSEL5 |= (1 << 26);		// DQMOUT1
+	PINSEL5 |= (1 << 28);		// DQMOUT2
+	PINSEL5 |= (1 << 30);		// DQMOUT3
+
+	EMC_CTRL = 1;				// Enable EMC
+
+	EMC_DYN_RD_CFG = 1;			// Configures the dynamic memory read strategy(Command delayed strategy)
+
+	/*
+	 * NOTE : All these are calculated using 72 MHz clock
+	 * i.e. 13.8889 ns period
+	 */
+
+	/*
+	 * CAS latency = 2
+	 * RAS latency(active to read/write delay) = 2
+	 */
+	EMC_DYN_RASCAS0 = 0x202;
+
+	/*
+	 * tRP = 20 ns
+	 * 1 + 1 clock cycles
+	 */
+	EMC_DYN_RP = 1;
+
+	/*
+	 * tRAS = 44 ns
+	 * 3 + 1 clock cycles
+	 */
+	EMC_DYN_RAS = 3;
+
+	/*
+	 * From the datasheet :
+	 * This value is normally found in SDRAM data sheets as tSREX, for devices
+	 * without this parameter you use the same value as tXSR.
+	 *
+	 * tXSR = 75 ns
+	 * 5 + 1 clock cycles
+	 */
+	EMC_DYN_SREX = 5;
+
+	/*
+	 * 2 + 1 clock cycle. (From BlueScreen SUN demo)
+	 */
+	EMC_DYN_APR = 2;
+
+	/*
+	 * 4 + 1 clock cycle. (From BlueScreen SUN demo)
+	 */
+	EMC_DYN_DAL = 4;
+
+	/*
+	 * 1 CK + 7.5 ns
+	 * 1 + 1 clock cycle
+	 */
+	EMC_DYN_WR = 1;
+
+	/*
+	 * 66 ns
+	 * 4 + 1 clock cyles
+	 */
+	EMC_DYN_RC = 4;
+	EMC_DYN_RFC = 4;
+
+	/*
+	 * tXSR = 75 ns
+	 * 5 + 1 clock cycles
+	 */
+	EMC_DYN_XSR = 5;
+
+	/*
+	 * 15 CK
+	 * 14 + 1 clock cyclces
+	 */
+	EMC_DYN_RRD = 14;
+
+	/*
+	 * 2 + 1 clock cycle. (From BlueScreen SUN demo)
+	 */
+	EMC_DYN_MRD = 2;
+
+	/* 256 MB (16Mx16), 4 banks, row length = 13, column length = 9 */
+	//EMC_DYN_CFG0 = 0x00000480;
+	/* 256 MB (16Mx16), 4 banks, row length = 13, column length = 9 */
+	EMC_DYN_CFG0 = 0x00004680;
+
+	// JEDEC General SDRAM Initialization Sequence
+	// DELAY to allow power and clocks to stabilize ~100 us
+	// NOP
+	EMC_DYN_CTRL = 0x0183;
+	//Issue SDRAM NOP (no operation) command ; CLKOUT runs continuously;All clock enables are driven HIGH continuously
+
+	for(i = 200*30; i>0; i--);
+
+	EMC_DYN_CTRL = 0x00000103;	//Issue SDRAM PALL (precharge all) command.
+	EMC_DYN_RFSH = 1;			//Indicates 1X16 CCLKs between SDRAM refresh cycles.
+	for(i = 128; i>0; --i);		// > 128 clk
+
+	/*
+	 * Indicates 35*16 CCLKs between SDRAM refresh cycles.
+	 * 64 ms, 8192-cycle
+	 */
+	EMC_DYN_RFSH = 35;
+
+	EMC_DYN_CTRL = 0x00000083;	//Issue SDRAM MODE command.
+
+	/* Set mode register in SDRAM */
+		/* Mode regitster table for Micron's MT48LCxx */
+		/* bit 9:   Programmed burst length(0)
+		 bit 8~7: Normal mode(0)
+		 bit 6~4: CAS latency 3
+		 bit 3:   Sequential(0)
+		 bit 2~0: Burst length is 4 for 32-bit bus
+		 row position is 13 */
+		//dummy = *((volatile unsigned short *)(SDRAM_BASE_ADDR | (0x23 << 12)));
+	/*
+	 * Set mode register in SDRAM
+	 * Mode regitster table for Micron's MT48LCxx
+	 * bit 9:   Programmed burst length(0)
+	 * bit 8~7: Normal mode(0)
+	 * bit 6~4: CAS latency 2
+	 * bit 3:   Sequential(0)
+	 * bit 2~0: Burst length is 4 for 32-bit bus
+	 * row position is 13
+	 */
+	wtemp = *((volatile unsigned long *)(DYNAMIC_MEM0_BASE | (0x22 << 13)));
+
+	EMC_DYN_CTRL = 0x0000;		//Issue SDRAM norm command ; CLKOUT stop;All clock enables low
+
+	EMC_DYN_CFG0 |= 0x80000;	//Buffer enabled for accesses to DCS0 chip
+
+
 }
 
 /******************************************************************************
