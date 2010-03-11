00eacea12a02618119b5edcaa36b93b177c41368
francoisgervais <francoisgervais@a08e5c7e-efe4-11de-a198-37193048d1ed>
2010-03-11 19:57:11 +0000

    First game level

---
+++ src/engine/levels/Level1.cpp
@@ -0,0 +1,21 @@
+/*
+ * Level1.cpp
+ *
+ *  Created on: Mar 11, 2010
+ *      Author: fgervais
+ */
+
+#include "Level1.h"
+
+Level1::Level1()
+	: Environment(480, 240, 24, 24) {
+
+}
+
+Level1::~Level1() {
+
+}
+
+void Level1::build() {
+
+}
