ab8269da380a7e98a79dc7e1ee57417344017a1e
francoisgervais <francoisgervais@a08e5c7e-efe4-11de-a198-37193048d1ed>
2010-03-13 18:55:39 +0000

    Added the gatter/scatter DMA struct and the DMAConfiguration class

--- trunk/src/peripheral/DMAChannel.h
+++ trunk/src/peripheral/DMAChannel.h
@@ -11,10 +11,27 @@
 #include "Peripheral.h"
 #include <stdint.h>
 
+/**
+ * This is the structure used for scatter/gatter DMA tranfer.
+ *
+ * The naming convention is the one used in the LPC24XX
+ * user manual.
+ */
+struct DMACCxLLI {
+	uint32_t DMACCxSrcAddr;
+	uint32_t DMACCxDestAddr;
+	DMACCxLLI* DMACCxLLI;
+	uint32_t DMACCxControl;
+};
+
+class DMAConfiguration;
+
 class DMAChannel: public Peripheral {
 public:
 	DMAChannel(uint8_t id);
 	virtual ~DMAChannel();
+
+	void configure(DMAConfiguration config);
 };
 
 #endif /* DMACHANNEL_H_ */
