dd538955b319c56e58d6e68fbee92e307dd72e6a
francoisgervais <francoisgervais@a08e5c7e-efe4-11de-a198-37193048d1ed>
2010-05-14 00:00:43 +0000

    The loopFirstFrame thing has been removed from HeroState. It seemed a good idea at first but on second though it's not. So now the update function of HeroState is empty. Anyway the only two state that were using that were HeroRunningLeft and Right.

--- trunk/src/engine/states/HeroState.cpp
+++ trunk/src/engine/states/HeroState.cpp
@@ -19,7 +19,6 @@ HeroState::HeroState(uint32_t width, uint32_t height, Bitmap** frames, uint32_t 
 	this->frames = frames;
 	this->numberOfFrame = numberOfFrame;
 	this->currentFrame = 0;
-	this->loopFirstFrame = 0;
 
 	// Ensure every bitmap are loaded into memory
 	for(uint32_t frameNumber=0; frameNumber<numberOfFrame; frameNumber++) {
@@ -43,46 +42,12 @@ uint32_t HeroState::getHeight() {
 }
 
 /**
- * Set the first frame of the frame loop.
- *
- * Sometime the first frame should be displayed only once when
- * entering the state. In that case, the first frame of the
- * loop should be set to the second frame i.e. frame1.
- *
- * The class default to frame0 for the loop first frame.
- *
- * @param frameNumber Loop first frame number.
- */
-void HeroState::setLoopFirstFrame(uint32_t frameNumber) {
-	loopFirstFrame = frameNumber;
-}
-
-/**
  * Reset the state.
  *
  * Currently only reset the frame counter to 0.
  */
 void HeroState::reset() {
 	currentFrame = 0;
-}
-
-/**
- * Update the current state.
- *
- * This function should be called on every engine tick. Even if this
- * function has to be overridden by some subclass, it should be called
- * anyway by the subclass as it update the frame pointer. It doesn't
- * matter if you call it at the beginning or at the end of your functions.
- *
- * @param sprite Sprite which should be updated by the state.
- */
-void HeroState::update(Hero* sprite) {
-	if(currentFrame < (numberOfFrame-1)) {
-		currentFrame++;
-	}
-	else {
-		currentFrame = loopFirstFrame;
-	}
 }
 
 void HeroState::render(Hero* sprite, VideoMemory* videoMemory) {
