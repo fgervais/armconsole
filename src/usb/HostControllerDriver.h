a267fb0a05cd5405c990a66c8621bf4c5d37fcb4
francoisgervais <francoisgervais@a08e5c7e-efe4-11de-a198-37193048d1ed>
2010-06-15 16:18:25 +0000

    Now able to do basic USB transfer

--- src/usb/HostControllerDriver.h
+++ src/usb/HostControllerDriver.h
@@ -13,31 +13,41 @@
  * the Linux implementation of OHCI.
  */
 
+#define USB_MEMORY 0x7FD00000
+
 #include "LPC23xx.h"
 #include <stdint.h>
 
 class UsbDevice;
 
-struct HcEd {                       /* ----------- HostController EndPoint Descriptor ------------- */
-    volatile  uint32_t  Control;              /* Endpoint descriptor control                              */
-    volatile  uint32_t  TailTd;               /* Physical address of tail in Transfer descriptor list     */
-    volatile  uint32_t  HeadTd;               /* Physcial address of head in Transfer descriptor list     */
-    volatile  uint32_t  Next;                 /* Physical address of next Endpoint descriptor             */
+// Completion codes
+#define CC_NOERROR	0
+
+// Transaction tokens
+#define PID_SETUP	0x00
+#define PID_OUT		0x01
+#define PID_IN		0x02
+
+struct HcEd {                       	/* ----------- HostController EndPoint Descriptor ------------- */
+    volatile  uint32_t  Control;		/* Endpoint descriptor control                              */
+    volatile  uint32_t  TailTd;			/* Physical address of tail in Transfer descriptor list     */
+    volatile  uint32_t  HeadTd;			/* Physcial address of head in Transfer descriptor list     */
+    volatile  uint32_t  Next;			/* Physical address of next Endpoint descriptor             */
 };
 
-struct HcTd {                       /* ------------ HostController Transfer Descriptor ------------ */
-    volatile  uint32_t  Control;              /* Transfer descriptor control                              */
-    volatile  uint32_t  CurrBufPtr;           /* Physical address of current buffer pointer               */
-    volatile  uint32_t  Next;                 /* Physical pointer to next Transfer Descriptor             */
-    volatile  uint32_t  BufEnd;               /* Physical address of end of buffer                        */
+struct HcTd {                       	/* ------------ HostController Transfer Descriptor ------------ */
+    volatile  uint32_t  Control;		/* Transfer descriptor control                              */
+    volatile  uint32_t  CurrBufPtr;		/* Physical address of current buffer pointer               */
+    volatile  uint32_t  Next;			/* Physical pointer to next Transfer Descriptor             */
+    volatile  uint32_t  BufEnd;			/* Physical address of end of buffer                        */
 };
 
-struct Hcca {                       /* ----------- Host Controller Communication Area ------------  */
-    volatile  uint32_t  IntTable[32];         /* Interrupt Table                                          */
-    volatile  uint32_t  FrameNumber;          /* Frame Number                                             */
-    volatile  uint32_t  DoneHead;             /* Done Head                                                */
-    volatile  uint32_t  Reserved[116];        /* Reserved for future use                                  */
-    volatile  uint32_t  Unknown[4];           /* Unused                                                   */
+struct Hcca {                      	 	/* ----------- Host Controller Communication Area ------------  */
+    volatile  uint32_t  IntTable[32];	/* Interrupt Table                                          */
+    volatile  uint32_t  FrameNumber;	/* Frame Number                                             */
+    volatile  uint32_t  DoneHead;		/* Done Head                                                */
+    volatile  uint32_t  Reserved[116];	/* Reserved for future use                                  */
+    volatile  uint32_t  Unknown[4];		/* Unused                                                   */
 };
 
 struct RootHubPort {
@@ -59,11 +69,55 @@ public:
 	void hcInterrupt();
 private:
 	Hcca* hcca;
+	HcTd* headTd;
+	HcTd* tailTd;
+	HcEd* ctrlEd;
+	HcEd* intInEd;
+	HcEd* intOutEd;
+	uint8_t* tdBuffer;
+	uint8_t* userBuffer;
+
 	OHCI_Typedef* ohciRegisters;
 
-	RootHubPort rootHubPort[MAX_ROOT_PORTS];
+	volatile RootHubPort rootHubPort[MAX_ROOT_PORTS];
+
+	// Global flags
+	volatile uint8_t transferCompleted;
 
 	void init();
+
+	// USB control functions
+	void getDescriptor(uint16_t descriptorTypeIndex, uint16_t descriptorLength, uint8_t* receiveBuffer);
+	void setAddress();
+	void setConfiguration();
+
+	void controlRead(uint8_t bmRequestType,
+			uint8_t bRequest,
+			uint16_t wValue,
+			uint16_t wIndex,
+			uint16_t wLength,
+			uint8_t* receiveBuffer);
+
+	void controlWrite(uint8_t bmRequestType,
+			uint8_t bRequest,
+			uint16_t wValue,
+			uint16_t wIndex,
+			uint16_t wLength);
+
+	uint8_t setupTransaction(HcEd* ed,
+			uint8_t bmRequestType,
+			uint8_t bRequest,
+			uint16_t wValue,
+			uint16_t wIndex,
+			uint16_t wLength);
+
+	uint8_t inTransaction(HcEd* ed, uint8_t* receiveBuffer, uint32_t transactionLength);
+	uint8_t outTransaction(HcEd* ed, uint8_t* transmitBuffer, uint32_t transactionLength);
+
+	uint8_t launchTransaction(HcEd* ed, uint32_t token, uint8_t* transmitBuffer, uint32_t transactionLength);
+
+	void enqueueED(HcEd* ed);
+	void enqueueTD(HcTd* td);
 };
 
 #endif /* HOSTCONTROLLERDRIVER_H_ */
